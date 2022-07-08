448f5e93b75fbf875ab7806bbfee214ba3fda273
Angel VI <a.v.iliev13@gmail>
2022-07-08 15:52:42 +0300

    add checking for required folder structure

--- main.go
+++ main.go
@@ -2,22 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 )
 
-func FilePathWalkDir(root string) ([]string, error) {
-    var files []string
-    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-        if !info.IsDir() {
-            files = append(files, path)
-            fmt.Println("File ", info.Name())
-        } else {
-        	fmt.Println("Dir ", info.Name())
-        }
-        return nil
-    })
-    return files, err
+func GetFilesFromDir(root string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	return files, err
+}
+
+func CheckDirExists(path string) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		log.Fatalf("'%s' folder does not exist. Please create it!", filepath.Base(path))
+	}
+}
+
+func GetPassedTests(path string) []string {
+	passedTestIds := []string{}
+	files, err := GetFilesFromDir(path)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(files)
+	return passedTestIds
 }
 
 func main() {
@@ -26,10 +40,16 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("Running script in: \t%s\n\n", root)
-	
-	files, err := FilePathWalkDir(root)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println(files[0])
+
+	// TODO: remove remaining_tests.xml cause it will be regenerated
+
+	passedPath := filepath.Join(root, "passed")
+	failedPath := filepath.Join(root, "failed")
+
+	// Make sure expected paths are created
+	CheckDirExists(passedPath)
+	CheckDirExists(failedPath)
+
+	passedTestIds := GetPassedTests(passedPath)
+	log.Println(passedTestIds)
 }
